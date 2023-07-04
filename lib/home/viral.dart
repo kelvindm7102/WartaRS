15c51ee658333831ccbf47502fe1124acc638435
Kevinn <kelvindm7102@gmail.com>
2023-07-04 18:56:58 +0700

    halaman pilih berita

---
+++ lib/home/viral.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:wartars/CardItem.dart';
+import 'package:wartars/dummy.dart';
+import 'package:wartars/home/detail.dart';
+
+class Viral extends StatelessWidget {
+  const Viral({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: Dummy.viral.length,
+          itemBuilder: (context, index) {
+          var form = Dummy.viral[index];
+          return CardItem(
+              date:"${form['date']}", 
+              thumbnail:"${form['thumb']}", 
+              title: "${form['title']}", 
+              onTap: (){
+                Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailBerita(detail: form),
+                ),
+              );
+              }
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
